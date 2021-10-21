e7ab0f88778c7f38da06b5111af69ad0b5150ab3
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-10-21 16:59:23 +0300

    js,hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
+const btnHamburger=document.querySelector('.btn-hamburger');
+const navHamburger=document.querySelector('.nav-hamburger');
+
+btnHamburger.addEventListener('click',handleToggleNavHamburger);
 
 
+function handleToggleNavHamburger(e){
+    
+    navHamburger.classList.toggle('on');
+
+}
